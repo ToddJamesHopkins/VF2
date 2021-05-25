7e9800578be7223540f0f2a2518d822de1f860bd
Jackson Currie <jackson.currie@radfords.co.nz>
2021-05-25 12:54:07 +1200

    Add request code

--- MobileTestApp1/MobileTestApp1/Views/MyAccount.xaml.cs
+++ MobileTestApp1/MobileTestApp1/Views/MyAccount.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using MobileTestApp1.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using Newtonsoft.Json;
+using System.Net.Http;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,6 +13,30 @@ namespace MobileTestApp1.Views
         {
             InitializeComponent();
             this.BindingContext = new MyAccountViewModel();
+        }
+
+        private async void ContentPage_Appearing(object sender, System.EventArgs e)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("http://localhost:5000/account/532543");
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Result>(stringResponse);
+            Credit.Text = result.Credit_Balance_.ToString();
+        }
+
+        private class Result
+        {
+            public int CustomerID { get; set; }
+            public int Mobile_or_Landline_Number_ { get; set; }
+            public string Account_Password_ { get; set; }
+            public decimal Credit_Balance_ { get; set; }
+            public int Minutes_Balance { get; set; }
+            public int Data_Balance_In_MB { get; set; }
+            public bool Allow_Credit_transfer_request { get; set; }
+            public bool On_contract { get; set; }
+            public bool Allow_Notifications { get; set; }
+            public int Current_Notification_Count { get; set; }
+            public bool Is_Transfer_Verified { get; set; }
         }
     }
 }
