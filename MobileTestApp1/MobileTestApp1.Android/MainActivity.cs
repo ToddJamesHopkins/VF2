3f5606b3a69643e22320d7820eee117eb4e0a672
Jackson Currie <jackson.currie@radfords.co.nz>
2021-05-27 09:14:44 +1200

    Add launcher icons

--- MobileTestApp1/MobileTestApp1.Android/MainActivity.cs
+++ MobileTestApp1/MobileTestApp1.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.OS;
 
 namespace MobileTestApp1.Droid
 {
-    [Activity(Label = "MobileTestApp1", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Vodafone Transfer", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
