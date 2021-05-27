273f3741ffa945903a123c7874336f67ac38748e
EricArther <ericarther_fish@icloud.com>
2021-05-27 13:33:19 +1200

    mess around

--- MobileTestApp1/MobileTestApp1/ViewModels/MyAccountViewModel.cs
+++ MobileTestApp1/MobileTestApp1/ViewModels/MyAccountViewModel.cs
@@ -1,14 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
 
 namespace MobileTestApp1.ViewModels
 {
     public class MyAccountViewModel : BaseViewModel
     {
+        public string NameText { get; set; }
+        public int MyProperty { get; }
+        public Command LoadedCommand { get; }
+
         public MyAccountViewModel()
         {
             Title = "My Account";
+            NameText = "One";
+
+            LoadedCommand = new Command(async() => await ExecuteCommand());
+        }
+
+        static HttpClient client = new HttpClient();
+
+        async Task ExecuteCommand()
+        {
+            Debug.WriteLine("note A");
+            HttpResponseMessage response = await client.GetAsync("https://vodafonecredittransfer20210525130039.azurewebsites.net/account/532543");
+
+            Account_Details_TBL accout = JsonConvert.DeserializeObject<Account_Details_TBL>(await response.Content.ReadAsStringAsync());
+            
+            NameText = $"Two";
+            Debug.WriteLine("note B " + NameText);
+        }
+
+        
+
+        
+
+        public class Account_Details_TBL
+        {
+            public int CustomerID { get; set; }
+            public int Mobile_or_Landline_Number_ { get; set; }
+            public string Account_Password_ { get; set; }
+            public decimal Credit_Balance_ { get; set; }
+            public int Minutes_Balance { get; set; }
+            public int Data_Balance_In_MB { get; set; }
+            public bool Allow_Credit_transfer_request { get; set; }
+            public bool On_contract { get; set; }
+            public bool Allow_Notifications { get; set; }
+            public int Current_Notification_Count { get; set; }
+            public bool Is_Transfer_Verified { get; set; }
         }
     }
 }
--- MobileTestApp1/MobileTestApp1/Views/MyAccount.xaml.cs
+++ MobileTestApp1/MobileTestApp1/Views/MyAccount.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using MobileTestApp1.ViewModels;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +20,39 @@ namespace MobileTestApp1.Views
         {
             InitializeComponent();
             this.BindingContext = new MyAccountViewModel();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            string a = await GetProductAsync(@"https://vodafonecredittransfer20210525130039.azurewebsites.net/account/532543");
+        }
+
+        static HttpClient client = new HttpClient();
+
+        async Task<string> GetProductAsync(string path)
+        {
+            HttpResponseMessage response = await client.GetAsync(path);
+
+            Account_Details_TBL accout = JsonConvert.DeserializeObject<Account_Details_TBL>(await response.Content.ReadAsStringAsync());
+
+            return $"name: {accout.Mobile_or_Landline_Number_}";
+
+        }
+
+        public class Account_Details_TBL
+        {
+            public int CustomerID { get; set; }
+            public int Mobile_or_Landline_Number_ { get; set; }
+            public string Account_Password_ { get; set; }
+            public decimal Credit_Balance_ { get; set; }
+            public int Minutes_Balance { get; set; }
+            public int Data_Balance_In_MB { get; set; }
+            public bool Allow_Credit_transfer_request { get; set; }
+            public bool On_contract { get; set; }
+            public bool Allow_Notifications { get; set; }
+            public int Current_Notification_Count { get; set; }
+            public bool Is_Transfer_Verified { get; set; }
         }
     }
 }
